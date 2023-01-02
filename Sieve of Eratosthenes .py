a87828a027ebf98f0d22784c403e91a825e32808
Harshit Singh Mahara <101328232+HarshitMahara@users.noreply.github.com>
2023-01-02 21:56:14 +0530

    Update Sieve of Eratosthenes .py

--- Sieve of Eratosthenes .py
+++ Sieve of Eratosthenes .py
@@ -2,7 +2,7 @@
 
 # sieve of eratosthenes is a simple and ancient algo used to find prime numbers upto any given limit . 
 #It is one of the most efficient ways to find small prime numbers for a given upper limit n , the algo. works by itratively marking the multiples of primes at composites.starting from 2 .
-#Once all multilples of 2 have been marked composite, the multiples of next primes , the process continues untill pxp<=n, p=current prime number .
+#Once all multilples of 2 have been marked composite, the multiples of next primes , the process continues untill p*p<=n, p=current prime number .
 # Following is the algo of the sieve of eratosthenes to find the prime numbers
 #1- To find out all primes under small n , generates the list of all integers from 2 to n .
 #2- Start with smallest prime numbers i.e, p=2.
